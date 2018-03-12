a8d6a0f9a88d3c324b341d25a5a9ac3d477ed3d3
Rian Quinn <rianquinn@gmail.com>
2018-03-12 15:47:32 -0600

    remove init / fini
    Signed-off-by: Rian Quinn <rianquinn@gmail.com>

--- newlib/libc/misc/fini.c
+++ newlib/libc/misc/fini.c
@@ -9,27 +9,3 @@
  * This file is distributed WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
-/* Handle ELF .{pre_init,init,fini}_array sections.  */
-#include <sys/types.h>
-
-#ifdef HAVE_INITFINI_ARRAY
-extern void (*__fini_array_start []) (void) __attribute__((weak));
-extern void (*__fini_array_end []) (void) __attribute__((weak));
-
-extern void _fini (void);
-
-/* Run all the cleanup routines.  */
-void
-__libc_fini_array (void)
-{
-  size_t count;
-  size_t i;
-  
-  count = __fini_array_end - __fini_array_start;
-  for (i = count; i > 0; i--)
-    __fini_array_start[i-1] ();
-
-  _fini ();
-}
-#endif
--- newlib/libc/misc/init.c
+++ newlib/libc/misc/init.c
@@ -9,35 +9,3 @@
  * This file is distributed WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
-/* Handle ELF .{pre_init,init,fini}_array sections.  */
-#include <sys/types.h>
-
-#ifdef HAVE_INITFINI_ARRAY
-
-/* These magic symbols are provided by the linker.  */
-extern void (*__preinit_array_start []) (void) __attribute__((weak));
-extern void (*__preinit_array_end []) (void) __attribute__((weak));
-extern void (*__init_array_start []) (void) __attribute__((weak));
-extern void (*__init_array_end []) (void) __attribute__((weak));
-
-extern void _init (void);
-
-/* Iterate over all the init routines.  */
-void
-__libc_init_array (void)
-{
-  size_t count;
-  size_t i;
-
-  count = __preinit_array_end - __preinit_array_start;
-  for (i = 0; i < count; i++)
-    __preinit_array_start[i] ();
-
-  _init ();
-
-  count = __init_array_end - __init_array_start;
-  for (i = 0; i < count; i++)
-    __init_array_start[i] ();
-}
-#endif
